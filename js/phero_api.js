22ec383c30d954b0cbbff65f44cae62e3d2b0406
almahfuz <almahfuz0327@gmail.com>
2022-09-02 19:21:26 +0600

    phero api

---
+++ js/phero_api.js
@@ -0,0 +1,55 @@
+const loadApi = async () => {
+    const url = 'https://openapi.programming-hero.com/api/course/curriculum';
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data.data);
+        showMilestones(data.data);
+    }
+    catch (err) { console.log(err); }
+}
+
+// show milestone function
+const showMilestones = data => {
+    const milestoneDetails = document.getElementById('milestone-details');
+    showImage(data[0].image);
+
+    data.forEach(milestone => {
+        const div = document.createElement('div');
+        // div.classList.add(['d-flex', 'justify-content-center']);
+        // div.classList.add('col-5');
+
+        milestone.modules.forEach(module => {
+            console.log(module.name)
+        })
+
+
+        div.innerHTML = `
+    <div class="accordion" id="accordionExample${milestone._id}">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne${milestone._id}">
+      <button class="accordion-button bg-danger text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne${milestone._id}" aria-expanded="true" aria-controls="collapseOne${milestone._id}" onclick="showImage('${milestone.image}')">
+      ${milestone.name}
+      </button>
+    </h2>
+    <div id="collapseOne${milestone._id}" class="accordion-collapse collapse show" aria-labelledby="headingOne${milestone._id}" data-bs-parent="#accordionExample${milestone._id}">
+      <div class="accordion-body">
+      ${milestone.modules.map(module => {
+            return `
+                            <p class="my-1 border"> ${module.name} </p>
+                        `
+        }).join("")}
+      </div>
+    </div>
+  </div>
+</div>
+    `
+        milestoneDetails.appendChild(div);
+    });
+
+}
+const showImage = image => {
+    const milestoneImage = document.getElementById('milestone-image');
+    milestoneImage.setAttribute("src", image);
+}
+loadApi();
